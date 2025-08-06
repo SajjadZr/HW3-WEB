6141f3dd5df659837a6dab718f68b58feea2149b
SajjaD <SajjadD2557@gmail.com>
2025-08-06 18:58:48 +0330

    update app.tsx for ui tags

--- paint/src/App/App.tsx
+++ paint/src/App/App.tsx
@@ -48,6 +48,40 @@ function App() {
     alert("paint loaded");
   };
 
+  const handleExport = () => {
+  const data = {
+    title,
+    boardData: shapes,
+  };
+  const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `${title}.json`;
+  a.click();
+  };
+
+
+  const handleImport = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const file = e.target.files?.[0];
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.onload = () => {
+    try {
+      const parsed = JSON.parse(reader.result as string);
+      if (!parsed.boardData) return alert("invalid format");
+      setTitle(parsed.title || "Imported");
+      setShapes(parsed.boardData);
+      alert("import successfully");
+    } catch (err) {
+      alert("error in reading file.");
+    }
+  };
+  reader.readAsText(file);
+  };
+
+
   return (
     <div className={styles.layout}>
       <Header title={title} setTitle={setTitle} shapes={shapes} setShapes={setShapes} />
@@ -55,6 +89,10 @@ function App() {
       <div style={{ padding: '1rem' }}>
         <button onClick={handleSave}>SAVE</button>
         <button onClick={handleLoad}>LOAD</button>
+        <button onClick={handleExport}>Json Output</button>
+        <label style={{ marginLeft: '1rem' }}>loading file
+        <input type="file" accept=".json" onChange={handleImport} />
+        </label>
       </div>
 
       <div className={styles.contents}>
